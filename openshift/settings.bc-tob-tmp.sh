5d4063e7cff81f03b6747120001aa1286ea5a2bd
Wade Barnes <wade.barnes@shaw.ca>
2018-09-20 15:22:51 -0700

    Add settings for the bc-tob-tmp prod environment.

---
+++ openshift/settings.bc-tob-tmp.sh
@@ -0,0 +1,17 @@
+# Setting for the TheOrgBook-BC environments.
+# Uses the existing `devex-von-tools` environment for builds and images
+export TOOLS="devex-von-tools"
+export PROJECT_NAMESPACE="devex-bcgov-dac"
+
+# The project components
+export components="tob-db tob-solr tob-api tob-web tob-wallet"
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Danger, Danger, Will Robinson!
+# ----------------------------------------------
+# Override environments, since there is only one:
+# devex-bcgov-dac-dev
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+export DEV="dev"
+export TEST="dev"
+export PROD="dev"
